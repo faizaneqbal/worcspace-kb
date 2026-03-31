4041f8b8bae94dc10d040519c3d6ee06611e2afb
Faizan Eqbal <faizaneqbal1999@gmail.com>
2026-03-31 14:34:36 +0530

    Initial setup: Vite + React project created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// App.jsx - This is the ROOT component of our app
+// Think of it as the main container that holds everything
+
+function App() {
+  return (
+    <div>
+      <h1>Worcspace Knowledge Base</h1>
+    </div>
+  )
+}
+
+export default App
